fa901930d419d4c03a629a8c15f9f2fd2255cac7
Florian Erwerth <erwerthflorian@outlook.de>
2022-10-15 07:46:08 +0200

    remove unused code, add router, add route to main page, add main page

---
+++ games/frontend/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {HashRouter, Routes, Route} from "react-router-dom";
+import {MainPage} from "./pages/MainPage";
+
+const root = ReactDOM.createRoot(
+   document.getElementById('root') as HTMLElement
+);
+root.render(
+   <React.StrictMode>
+      <HashRouter>
+         <Routes>
+            <Route element={<MainPage/>} path={"/"}></Route>
+         </Routes>
+      </HashRouter>
+   </React.StrictMode>
+);
